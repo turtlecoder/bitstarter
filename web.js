f7ba27b5441b38b6c8edb22026f80ac482421cf0
Haroon A. Khan <haroon.khan@gmail.com>
2013-07-07 23:05:00 +0000

    Using the index.html

--- web.js
+++ web.js
@@ -2,11 +2,19 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var buffer = fs.readFileSync('index.html');
+var console.log("Buffer: "+buffer.toString();
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
